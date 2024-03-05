c0ebef5c9f39a328e8cbe824b96fa970c9489fc0
Arecsu <alejandro9r@gmail.com>
2024-03-04 23:12:58 -0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,31 @@
 import { defineConfig } from 'astro/config';
 import alpinejs from "@astrojs/alpinejs";
 
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs()]
-});
+
+  integrations: [alpinejs()],
+  // site: 'https://arecsu.github.io',
+  // base: '/www',
+
+  vite: {
+    resolve: {
+      alias: {
+        '@/': `${path.resolve(__dirname, 'src')}/`
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/styles/vars.scss";`
+        }
+      }
+    }
+  }
+})
