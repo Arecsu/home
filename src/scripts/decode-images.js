d4d10353140447967d694ec6c1fb993c933a8957
Arecsu <alejandro9r@gmail.com>
2024-07-02 17:44:16 -0300

    decode-images only on desktop

--- src/scripts/decode-images.js
+++ src/scripts/decode-images.js
@@ -1,9 +1,17 @@
 const decodeImageEvent = (e) => {
-    e.target.decode()
-    e.target.removeEventListener('load', decodeImageEvent)
+	e.target.decode()
+	e.target.removeEventListener("load", decodeImageEvent)
 }
 
 document.querySelectorAll("img").forEach((img) => {
-    const isLoaded = img.complete && img.naturalHeight !== 0;
-    isLoaded ? img.decode() : img.addEventListener('load', decodeImageEvent)
+	const isLoaded = img.complete && img.naturalHeight !== 0
+	if (
+		!(
+			screen.orientation.type == "landscape-primary" ||
+			screen.orientation.type == "landscape-secondary"
+		)
+	)
+		return
+	console.log("decoding ", img.src)
+	isLoaded ? img.decode() : img.addEventListener("load", decodeImageEvent)
 })
