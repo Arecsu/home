2985e8bafc839c63ef4905c7e8f3ea3425d1da26
Arecsu <alejandro9r@gmail.com>
2024-07-09 12:20:45 -0300

    mobile-layout-breakpoint rename

--- src/scripts/decode-images.js
+++ src/scripts/decode-images.js
@@ -1,17 +1,14 @@
+const messageDecode = (img) => {
+	console.log("decoding ", img.src)
+}
+
 const decodeImageEvent = (e) => {
-	e.target.decode()
+	e.target.decode().then(messageDecode(e.target))
 	e.target.removeEventListener("load", decodeImageEvent)
 }
 
 document.querySelectorAll("img").forEach((img) => {
 	const isLoaded = img.complete && img.naturalHeight !== 0
-	if (
-		!(
-			screen.orientation.type == "landscape-primary" ||
-			screen.orientation.type == "landscape-secondary"
-		)
-	)
-		return
-	console.log("decoding ", img.src)
-	isLoaded ? img.decode() : img.addEventListener("load", decodeImageEvent)
+	if (!(screen.orientation.type == "landscape-primary" || screen.orientation.type == "landscape-secondary")) return
+	isLoaded ? img.decode().then(messageDecode(img)) : img.addEventListener("load", decodeImageEvent)
 })
