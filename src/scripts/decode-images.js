e3efac465d8074c3304411c7d86f19b6f9e5df35
Arecsu <alejandro9r@gmail.com>
2024-09-19 08:05:38 -0300

    decode-images actually work

--- src/scripts/decode-images.js
+++ src/scripts/decode-images.js
@@ -3,12 +3,17 @@ const messageDecode = (img) => {
 }
 
 const decodeImageEvent = (e) => {
-	e.target.decode().then(messageDecode(e.target))
+	// e.target.decode().then(messageDecode(e.target))
+	e.target.decode()
 	e.target.removeEventListener("load", decodeImageEvent)
 }
 
+const supportsHoverAndFinePointer = () => window.matchMedia('(hover: hover) and (pointer: fine)').matches
+
 document.querySelectorAll("img").forEach((img) => {
+	if (!supportsHoverAndFinePointer()) return
+	// if (!(screen.orientation.type == "landscape-primary" || screen.orientation.type == "landscape-secondary")) return
 	const isLoaded = img.complete && img.naturalHeight !== 0
-	if (!(screen.orientation.type == "landscape-primary" || screen.orientation.type == "landscape-secondary")) return
-	isLoaded ? img.decode().then(messageDecode(img)) : img.addEventListener("load", decodeImageEvent)
+	// isLoaded ? img.decode().then(messageDecode(img)) : img.addEventListener("load", decodeImageEvent)
+	isLoaded ? img.decode() : img.addEventListener("load", decodeImageEvent)
 })
