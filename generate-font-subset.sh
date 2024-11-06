04a5fe0d3919d80bd27d1bf8e66e521f6e66d030
Arecsu <alejandro9r@gmail.com>
2024-11-05 21:52:22 -0300

    new arrows in the subset!

--- generate-font-subset.sh
+++ generate-font-subset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm public/inter-4.0/*-subset.woff2 2> /dev/null || true \
- && glyphhanger http://localhost:4321 --subset="public/inter-4.0/*.woff2" --formats=woff2 --whitelist=↑ \
+ && glyphhanger http://localhost:4321 --subset="public/inter-4.0/*.woff2" --formats=woff2 --whitelist=↑↓ \
  && mv public/inter-4.0/*-subset.woff2 public/inter-4.0/subset
