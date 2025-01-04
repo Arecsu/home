27e877ab7a0d29de6b4b82e101311195a52b7d39
Arecsu <alejandro9r@gmail.com>
2025-01-04 16:16:04 -0300

    fixed global types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,12 @@
-import { type SupportedReflectionsLang } from "@/languages.ts"
-
-interface NowListening {
-   track: string
-   artist: string
-}
-type NowListeningPromise = Promise<NowListening | null>
+import { type SupportedReflectionsLang } from "@/languages.ts";
 
 declare global {
+   interface NowListening {
+      track: string;
+      artist: string;
+   }
+   type NowListeningPromise = Promise<NowListening | null>;
+
    interface Window {
       setLang: (lang: SupportedReflectionsLang) => void;
       setPreferredLang: (document: DocumentType) => void;
